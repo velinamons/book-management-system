6cd8bd4d9a26ee7d157bc71206fc25bf7f40c851
velinamons <velinamons@gmail.com>
2025-02-20 15:22:26 +0200

    Set up PostgreSQL db connection; Add .env.sample and modify config.py

---
+++ app/config.py
@@ -0,0 +1,14 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+POSTGRES_USER = os.getenv("POSTGRES_USER")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+POSTGRES_DB = os.getenv("POSTGRES_DB")
+POSTGRES_HOST = os.getenv("POSTGRES_HOST")
+POSTGRES_PORT = os.getenv("POSTGRES_PORT", "5432")
+
+DATABASE_URL = (
+    f"postgresql+psycopg2://{POSTGRES_USER}:{POSTGRES_PASSWORD}{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
+)
