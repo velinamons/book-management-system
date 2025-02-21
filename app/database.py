7f555a48b613d53b2a877c0c07ab956a9195339f
velinamons <velinamons@gmail.com>
2025-02-21 18:47:30 +0200

    Add init_db function in database.py; Init DB on FastAPI startup in main.py

--- app/database.py
+++ app/database.py
@@ -9,6 +9,10 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
 
+def init_db():
+    Base.metadata.create_all(bind=engine)
+
+
 def get_db():
     db = SessionLocal()
     try:
