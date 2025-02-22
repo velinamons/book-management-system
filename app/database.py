43ae914169dc3d556e12d63a6c315846cd0548e4
velinamons <velinamons@gmail.com>
2025-02-22 13:12:10 +0200

    Modify init_db: check db, tables existance; create db if db doesn't exist

--- app/database.py
+++ app/database.py
@@ -1,4 +1,5 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, inspect
+from sqlalchemy_utils import database_exists, create_database
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from app.config import DATABASE_URL
@@ -9,8 +10,19 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
 
-def init_db():
-    Base.metadata.create_all(bind=engine)
+def init_db() -> None:
+    if not database_exists(engine.url):
+        print("Database doesn't exist")
+        create_database(engine.url)
+        print("Database created successfully.")
+
+        inspector = inspect(engine)
+        tables = inspector.get_table_names()
+
+        if not tables:
+            print("No tables found. Please run the migrations.")
+        else:
+            print(f"Tables {tables} exist.")
 
 
 def get_db():
